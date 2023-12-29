1e482e8e71dc72baf1fb44e88ba27993f7bfc8dd
sudofinn <gufi127127@gmail.com>
2023-12-29 22:08:01 +0100

    Changed affiliate handle

--- pages/affiliates.tsx
+++ pages/affiliates.tsx
@@ -79,7 +79,7 @@ const Affiliates = () => {
            
        </section>  
 
-       <Link href="https://vimfinn.gumroad.com/affiliates">
+       <Link href="https://codingnotes.gumroad.com/affiliates">
        <button className="relative mx-auto flex align-start mt-4  items-center px-12 py-3 overflow-hidden text-2xl  text-white hover:bg-white hover:text-black border-white  border-4 rounded-md  group ">
               <span className="absolute left-0 block w-full h-0 transition-all  opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
               <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
